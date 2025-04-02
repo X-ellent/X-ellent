4d13f6bf559069cc0a681933554cba10ffcfd316
R E Broadley <rebroad@users.noreply.github.com>
2025-04-02 23:55:41 +0100

    Slow down framerate

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>  // For gettimeofday
 
 #include "fix.h"
 #include "message.h"
@@ -54,6 +55,10 @@ extern int main(int argc,char *argv[])
 {
     int ti;
     struct player *p;
+    struct timeval start_time, end_time;  // For frame timing
+    long frame_time_us;  // Frame time in microseconds
+    const long target_frame_time_us = 59000;  // Target ~17fps (59ms per frame)
+    
     saveall=0;
     chdir(LIB);
     DL("Entering main");
@@ -86,57 +91,72 @@ extern int main(int argc,char *argv[])
     sleepsave=0;
     setup_error_handler();
     while (1) {
-	for (p=playone;p;p=p->next) 
-	    if (p->connected) draw_all(p);
-	free_beams();
-	fire_starbursts();
-	for (p=playone;p;p=p->next) 
-	    if (p->playing) update_player(p);
-	update_turrets();
-	update_bonus();
-	update_teleports();
-	move_particles();
-	move_explosions();
-	move_objects();
-	update_trolleys();
-	do_collisions();
-	move_lifts();
-	frame++;
-	lastsave++;
-	if (!players) {
-	    sleep(1);
-	    sleepsave++;
-	    if (sleepsave==2) {
-		save_players();
-		save_mines();
-	    }
-	} else {
-	    sleepsave=0;
-	    if (lastsave>SAVE_TIME) {
-		save_players();
-		save_mines();
-		lastsave=0;
-	    }
-	}
-	if (saveall==-2) {
-	    saveall=0;
-	    global_message("Signal recieved: Saving all data...");
-	    save_players();
-	    save_mines();
-	}
-	if (saveall==-1) {
-	    global_message("Signal recieved: Shutting down.");
-	    save_players();
-	    save_mines();
-	    for (p=playone;p;p=p->next)
-		if (p->connected) {
-		    XAutoRepeatOn(p->d.disp);
-		    shutdown_display(p);
-		}
-	    exit(0);
-	}
-	do_login();
+        gettimeofday(&start_time, NULL);  // Get start time of frame
+        
+        for (p=playone;p;p=p->next) 
+            if (p->connected) draw_all(p);
+        free_beams();
+        fire_starbursts();
+        for (p=playone;p;p=p->next) 
+            if (p->playing) update_player(p);
+        update_turrets();
+        update_bonus();
+        update_teleports();
+        move_particles();
+        move_explosions();
+        move_objects();
+        update_trolleys();
+        do_collisions();
+        move_lifts();
+        frame++;
+        lastsave++;
+        
+        if (!players) {
+            sleep(1);
+            sleepsave++;
+            if (sleepsave==2) {
+                save_players();
+                save_mines();
+            }
+        } else {
+            sleepsave=0;
+            if (lastsave>SAVE_TIME) {
+                save_players();
+                save_mines();
+                lastsave=0;
+            }
+        }
+        
+        if (saveall==-2) {
+            saveall=0;
+            global_message("Signal recieved: Saving all data...");
+            save_players();
+            save_mines();
+        }
+        if (saveall==-1) {
+            global_message("Signal recieved: Shutting down.");
+            save_players();
+            save_mines();
+            for (p=playone;p;p=p->next)
+                if (p->connected) {
+                    XAutoRepeatOn(p->d.disp);
+                    shutdown_display(p);
+                }
+            exit(0);
+        }
+        do_login();
+        
+        // Frame rate limiter
+        gettimeofday(&end_time, NULL);
+        frame_time_us = (end_time.tv_sec - start_time.tv_sec) * 1000000 + 
+                       (end_time.tv_usec - start_time.tv_usec);
+        
+        if (frame_time_us < target_frame_time_us) {
+            // Sleep for the remaining time to achieve target frame rate
+            usleep(target_frame_time_us - frame_time_us);
+        }
     }
+    
     sleep(5);
     DL("Exiting");
     return 0;
