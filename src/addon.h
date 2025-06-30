e05e60ddd8eb31ffce58a8c4cea209fce8ef8e39
R E Broadley <rebroad@users.noreply.github.com>
2025-06-30 19:22:21 +0100

    whitespace

--- src/addon.h
+++ src/addon.h
@@ -18,39 +18,39 @@
 #include "fix.h"
 
 struct addtype {
-    struct addtype *next;     /* Pointer to next one */
-    int type;                 /* What addon type is this? */
-    char name[64];            /* What's it called */
-    char subs[4];             /* What subsystem name is this? */
-    int upgrades;             /* What level of upgrade does it have */
-    int damage;               /* How many damage points can it take */
-    int cost[MAX_UPGRADE];    /* How much does it cost per level */
-    int repcost;              /* What is the repair cost */
+	struct addtype *next;	  /* Pointer to next one */
+	int type;				  /* What addon type is this? */
+	char name[64];			  /* What's it called */
+	char subs[4];			  /* What subsystem name is this? */
+	int upgrades;			  /* What level of upgrade does it have */
+	int damage;				  /* How many damage points can it take */
+	int cost[MAX_UPGRADE];	  /* How much does it cost per level */
+	int repcost;			  /* What is the repair cost */
 };
 
 extern struct addtype *firstaddtype;
 
 struct addon {
-    struct addon *next;      /* Pointer to next one */
-    struct addtype *is;      /* Which kind of thing is this? */
-    int level;               /* What level is it at? */
-    int damage;              /* How many damage points does it have left? */
-    int info[MAX_INFO];      /* What info does it have about it eg. coords*/
+	struct addon *next;		 /* Pointer to next one */
+	struct addtype *is;		 /* Which kind of thing is this? */
+	int level;				 /* What level is it at? */
+	int damage;				 /* How many damage points does it have left? */
+	int info[MAX_INFO];		 /* What info does it have about it eg. coords*/
 };
 
 extern struct addon *freeaddon;
 
-#define ADD_COMPASS         1
-#define ADD_CLOAKING        2
-#define ADD_INVIS           3
-#define ADD_MINESWEEP       4
-#define ADD_ENERGY          5
-#define ADD_RADAR           6
-#define ADD_TARGET          7
-#define ADD_FUELTANK        8
-#define ADD_SHIELD          9
-#define ADD_VISUAL          10
-#define ADD_ANTICLOAK       11
+#define ADD_COMPASS			1
+#define ADD_CLOAKING		2
+#define ADD_INVIS			3
+#define ADD_MINESWEEP		4
+#define ADD_ENERGY			5
+#define ADD_RADAR			6
+#define ADD_TARGET			7
+#define ADD_FUELTANK		8
+#define ADD_SHIELD			9
+#define ADD_VISUAL			10
+#define ADD_ANTICLOAK		11
 
 void			init_addons();
 struct addtype*	find_addtyp(int typ);
@@ -60,6 +60,6 @@ struct addon*	add_addon(int typ);
 void			free_addon(struct addon *a);
 struct addon*	strip_addons(struct player *p,struct addon *a);
 void			new_addon_level(struct player *p,struct addon *a);
-void			addon_command(struct player *p,struct addon *a, uchar *s);
+void			addon_command(struct player *p,struct addon *a,uchar *s);
 
 #endif
